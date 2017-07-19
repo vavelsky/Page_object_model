b5492b1fa5a9aab286d011ddcb4b871e89e3d8b0
DimaKostiukevych <dima.kostiukevych@hotmail.com>
2017-07-19 09:12:01 +0200

    Pitched - Database screen refactor

--- src/main/java/com/testobject/screens/Create/DatabasePage.java
+++ src/main/java/com/testobject/screens/Create/DatabasePage.java
@@ -1,29 +1,50 @@
 package com.testobject.screens.Create;
 
 import com.testobject.screens.StaticPages.AbstractScreen;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class DatabasePage extends AbstractScreen{
-git config user.name
+
     @FindBy(xpath = "//database/div/div/div[1]/h1")
     private WebElement databasePageTitle;
 
-    By emptyPlaylistlist = By.xpath("//playlists-table/table/tbody/tr[2]/td/p");
-    By FilterByParrentBrand = By.xpath("");
-    By FilterByPlaylistBrand = By.xpath("");
-    By FilterByTeam = By.xpath("");
-    By Notification = By.xpath("//body/md-toast/div/span");
-    By Playlist = By.xpath("//tr[@class='playlist-row ng-scope']/td/a");
-    By playlistNameOnList = By.xpath("//playlists-table/table/tbody/tr[3]/td[1]/a");
-    By playlistStatus = By.xpath("//playlists-table/table/tbody/tr[3]/td[6]/span/span");
-    By publishButton = By.xpath("//button[contains(., 'Publish')]");
-    By searchButton = By.xpath("//button[contains(., 'Search')]");
-    By SearchInput = By.xpath("//input[@type='text']");
-    By ShowMoreButton = By.xpath("//ui-view/database/div/div/div[4]/button");
+    @FindBy(xpath = "//playlists-table/table/tbody/tr[2]/td/p")
+    private WebElement emptyPlaylistlist;
 
+    @FindBy(xpath = "//playlists-table/table/tbody/tr[2]/td/p")
+    private  WebElement filterByParrentBrand;
+
+    @FindBy(xpath = "")
+    private WebElement filterByPlaylistBrand;
+
+    @FindBy(xpath = "")
+    private WebElement filterByTeam;
+
+    @FindBy(xpath = "//body/md-toast/div/span")
+    private WebElement Notification;
+
+    @FindBy(xpath = "//tr[@class='playlist-row ng-scope']/td/a")
+    private WebElement Playlist;
+
+    @FindBy(xpath = "//playlists-table/table/tbody/tr[3]/td[1]/a")
+    private WebElement playlistNameOnList;
+
+    @FindBy(xpath = "//playlists-table/table/tbody/tr[3]/td[6]/span/span")
+    private WebElement playlistStatus;
+
+    @FindBy(xpath = "//button[contains(., 'Publish')]")
+    private WebElement publishButton;
+
+    @FindBy(xpath = "//button[contains(., 'Search')]")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//input[@type='text']")
+    private WebElement searchInput;
+
+    @FindBy(xpath = "//ui-view/database/div/div/div[4]/button")
+    private WebElement showMoreButton;
 
     public DatabasePage(WebDriver driver) {super(driver);}
 
